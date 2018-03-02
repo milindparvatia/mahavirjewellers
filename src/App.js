6f6e5118c5a980d2bcdb2278d6897f2e5828a171
Milind Parvatia <milind1997@gmail.com>
2018-03-02 14:46:45 +0530

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Slider from './components/Slider/Slider';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Slider />
       </div>
+  </MuiThemeProvider>
     );
   }
 }
