d17a417f183ef64a57da4bbd027810cf56ada506
Milind Parvatia <33784561+milindparvatia@users.noreply.github.com>
2018-03-06 13:16:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import firebase from 'firebase';
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCedhySAEHZQDRA9dGqAvXUktqIhis9PN0",
-    authDomain: "mahavirjewellers-3f88b.firebaseapp.com",
-    databaseURL: "https://mahavirjewellers-3f88b.firebaseio.com",
-    projectId: "mahavirjewellers-3f88b",
-    storageBucket: "mahavirjewellers-3f88b.appspot.com",
-    messagingSenderId: "205551242273"
-  };
-  firebase.initializeApp(config);
 
 ReactDOM.render(
     <App />, 
